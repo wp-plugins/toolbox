bca4008782f313f1b6b4950df66585fd94b6c103
sergej.mueller <sergej.mueller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-10 05:46:52 +0000

    Add copyright & license information
    git-svn-id: https://plugins.svn.wordpress.org/toolbox/trunk@929368 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- toolbox.php
+++ toolbox.php
@@ -5,7 +5,26 @@ Plugin URI: http://playground.ebiene.de/toolbox-wordpress-plugin/
 Description: Ersetzt oder erweitert die <em>functions.php</em> im Theme, indem Code-Schnipsel als Toolbox-Module angelegt und direkt im Plugin verwaltet werden.
 Author: Sergej M&uuml;ller
 Author URI: http://wpcoder.de
+License: GPLv2 or later
 Version: 0.1
+*/
+
+/*
+Copyright (C)  2011-2014 Sergej Müller
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 
